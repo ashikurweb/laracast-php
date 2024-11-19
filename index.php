7d2722dd5f18e570d1b2d195fe6ca8a85a0a74e4
ashikurweb <ashikurrahman7194@gmail.com>
2024-11-20 01:24:16 +0600

    variable declare

--- index.php
+++ index.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laracast Tutotial || Code With By Ashikur Rahman</title>
 </head>
+<style> 
+    body {
+        display: grid;
+        place-items: center;
+        min-height: 100vh;
+        margin: 0;
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
 <body>
-    <h1>
-        <?php
-        $greeting = "Hello";
-        // echo "{$greeting} Everybody!"; # code excute right
-        // echo $greeting . ' ' . 'Everybody!'; # code excute right
-        // echo "$greeting Everybody!"; #code excute right
-        ?>
-    </h1>
+    <?php
+     $name = "Dark Matter" 
+    ?>
+    <h1>You have read <?php echo "'{$name}'" ?></h1>
 </body>
 </html>
