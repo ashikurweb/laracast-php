36a0064fe9359e6982d23cba1714b3447520a1ef
ashikurweb <ashikurrahman7194@gmail.com>
2024-11-20 18:24:59 +0600

    Seperate Login From the Templete

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laracast Tutotial || Code With By Ashikur Rahman</title>
-</head>
-
-<body>
-    <h1>Recommended Books</h1>
-
-    <?php
+<?php
     $books = [
         [
             'name'          => 'Making India Awesome',
@@ -45,15 +34,6 @@
         return $book['author'] === 'Naipaul';
     });
 
-    ?>
 
-    <ul>
-       <?php foreach ( $filteredBooks as $book ) : ?>
-        <a href="<?php echo $book['purchaseUrl'] ?>">
-            <li><?php echo $book['name'] ?> - <?php echo $book['author'] ?>(<?php echo $book['releaseYear'] ?>) </li>
-        </a>
-      <?php endforeach; ?>
-    </ul>
+    require('index.view.php');
 
-</body>
-</html>
