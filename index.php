e63d5aeb1db0e9dd52f3f0a01ccbc4d11dd52f19
ashikurweb <ashikurrahman7194@gmail.com>
2024-11-22 17:46:09 +0600

    Environments and Configuration Flexibility

--- index.php
+++ index.php
@@ -3,29 +3,15 @@ require 'views/partials/head.php';
 
 require 'functions.php';
 
+require 'DB/Database.php';
+
 // require 'router.php';
 
 // Connect to our MySql database.
 
-class Database {
+$config = require('config.php');
 
-    public $connection;
+$db = new Database( $config['database'] );
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
-    public function __construct()
-    {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=valet;password=valet;charset=utf8mb4";
-        $this->connection = new PDO( $dsn );
-    }
-    public function query ( $query ) {
-
-        $statement = $this->connection->prepare( $query );
-
-        $statement->execute();
-        return $statement;
-    }
-}
-
-$db = new Database();
-$post = $db->query("SELECT * FROM posts WHERE id = 1")->fetchAll(PDO::FETCH_ASSOC);
-
-dd($post[0]['title']);
+dd( $posts );
